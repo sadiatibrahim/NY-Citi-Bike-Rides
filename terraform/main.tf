8d3370e495f145e1959bd06cc65ca1fcc6ac7295
sadiatibrahim <57956925+sadiatibrahim@users.noreply.github.com>
2024-03-31 18:54:23 +0000

    HCL code to provision resources in gcp

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "citi-bikes-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "citi-rides-dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
+
+resource "google_compute_instance" "instance_1" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.size
+    }
+  }
+
+  network_interface {
+    network = var.network
+  }
+
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,78 @@
+variable "credentials" {
+  description = "my-credentials"
+  default     = "patg to your service account"
+
+}
+
+variable "project" {
+  description = "NYC-Bike-Rides"
+  default     = "your_project_id" #your project id
+
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+
+}
+
+variable "zone" {
+  description = "zone"
+  default     = "us-central1-a"
+
+}
+
+variable "location" {
+  description = "project-location"
+  default     = "US"
+
+}
+
+variable "bq_dataset_name" {
+  description = "Big query dataset name"
+  default     = "nyc_citi_bike_rides"
+
+}
+
+variable "gcs_bucket_name" {
+  description = "Storage bucket name"
+  default     = "sadiat_citi_bike_rides_bucket"
+
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+
+}
+
+variable "instance_name" {
+  description = "name of compute engine instance"
+  default     = "instance-1"
+
+}
+
+
+variable "machine_type" {
+  description = "type of compute engine"
+  default     = "e2-standard-4"
+
+}
+
+variable "image" {
+  description = "VM image"
+  default     = "ubuntu-2004-focal-v20240125"
+
+}
+
+variable "size" {
+  description = "storage size for my vm instance"
+  default     = "30"
+
+}
+
+variable "network" {
+  description = "my instance network"
+  default     = "default"
+
+}
